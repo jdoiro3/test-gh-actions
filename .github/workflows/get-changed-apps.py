55d30bd91aa85fc98b4f531f2025ff8a053093e3
jdoiro3 <josephdoiron1234@yahoo.com>
2024-06-10 08:40:50 -0400

    added script

---
+++ .github/workflows/get-changed-apps.py
@@ -0,0 +1,8 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("changed_files", type=str)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    print([v.strip() for v in args.changed_files.split(",")])
